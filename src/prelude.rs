e0337467809bc3d29e9e9f4948f119cf2eaa2cb5
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2025-07-31 11:31:41 -0700

    Changed sketch paths to 2018 rust modules

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,2 @@
-pub use crate::sketches::moving_circle::*;
-pub use crate::sketches::polygon::*;
+pub use crate::sketches::moving_circle::moving_circle_main;
+pub use crate::sketches::polygon::polygon_main;
